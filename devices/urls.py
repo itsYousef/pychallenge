e889dd1e573ad82b5c56abbd677f970c9f50f029
Yousef Alamdarlou <yousefalamdarlou@gmail.com>
2023-10-02 09:07:03 +0330

    name added for url patterns

--- devices/urls.py
+++ devices/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DeviceListApiView, DeviceDetailApiView
 
 urlpatterns = [
-    path('', DeviceListApiView.as_view()),
-    path('<str:device_id>/', DeviceDetailApiView.as_view()),
+    path('', DeviceListApiView.as_view(), name='device_list'),
+    path('<str:device_id>/', DeviceDetailApiView.as_view(), name='device_detail'),
 ]
